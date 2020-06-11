48095add893a21e93f46325a2b1800c58bc2b8ce
reagan <Hieu1407@live.missouristate.edu>
2020-06-12 00:24:00 +0700

    Finished custom email validator. Wrote tests for email length and case_sensitivity. Minor adjustment to models/user.rb

--- app/models/rsvpq.rb
+++ app/models/rsvpq.rb
@@ -1,25 +1,13 @@
-require 'mail'
-
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    email = Mail::Address.new(value)
-    unless email.address =~ Devise.email_regexp
-      record.errors[attribute] << (options[:message] || "is not an email")
-    end
-  rescue Mail::Field::ParseError => e
-    record.errors[attribute] << (options[:message] || "is not an email")
-  end
-end
-
 class Rsvpq < ApplicationRecord
+  before_save {email.downcase!}
   belongs_to :user, :optional => true
   belongs_to :event
 #  validates :user_id, presence: true
-	validates :event_id, presence: true
-	
-	validates :email, presence: {unless: :user_id? }, 
+  validates :event_id, presence: true
+  
+  validates :email, presence: {unless: :user_id? }, 
+             length: {in: 3..254},
              allow_blank: true, 
-             length: {minimum: 3, maximum:254},
+             uniqueness: {case_sensitive: false, scope: :event_id},
              email: true
-
 end
--- test/models/rsvpq_test.rb
+++ test/models/rsvpq_test.rb
@@ -2,27 +2,43 @@ require 'test_helper'
 
 class RsvpqTest < ActiveSupport::TestCase
 
-  def setup
-    @rsvpq = rsvpqs(:one)
+  setup do
+    @rsvpqT = Rsvpq.find(1)
   end
 
-  [:event_id].each do |field|
-    test "#{field}_must_not_be_empty" do
-      @rsvpq.send "#{field}=", nil
-      refute @rsvpq.valid?
-      refute_empty @rsvpq.errors[field]
-    end
-    end
-  [:email].each do |field|
-    test 'test_email' do
-    email_entry =  'this@@@@email@@@@.wontwork.'
-    @rsvpq.send "#{field}=", email_entry
-    refute @rsvpq.valid?
-    end
+  test "event_id_must_not_be_empty" do
+    @rsvpqT.event_id = nil
+    assert_not @rsvpqT.valid?, "Empty event_id accepted"
 
-    test 'test_email_length' do
-      @rsvpq.send "#{field}=", "#{field}" * 255
-      refute @rsvpq.valid?
-    end
-end
+    @rsvpqT.event_id = rsvpqs(:valid_rsvpq_two).event_id
+    assert @rsvpqT.valid?, "Valid event_id not accepted"
+  end
+
+  test 'test_email' do
+    #Email format test
+    email_entry =  'this@@@@email@@@@.wontwork.'
+    @rsvpqT.email = email_entry
+    assert_not @rsvpqT.valid?, "Invalid email format accepted"
+    #Email presence test
+    @rsvpqT.email = "thisIsmyEmail@gmail.com"
+    @rsvpqT.user_id = nil
+    assert @rsvpqT.valid?, "email present when user_id absent should be valid"
+  end
+
+  test 'test_email_length over 254 chars' do
+    @rsvpqT.email = "a" * 245 + "@example.com"
+    assert_not @rsvpqT.valid?, "email is over 254 characters"
+  end
+
+  test 'test_email_length equals 254 chars' do
+    @rsvpqT.email = "a" * 242 + "@example.com"
+    assert @rsvpqT.valid?, "email at the limit should be valid"
+  end
+
+  test 'test_email_case_sensitive' do
+    mixed_case_email = "AcB@ExAmpLe.cOm"
+    @rsvpqT.email = mixed_case_email
+    @rsvpqT.save
+    assert_equal mixed_case_email.downcase, @rsvpqT.reload.email
+  end
 end
